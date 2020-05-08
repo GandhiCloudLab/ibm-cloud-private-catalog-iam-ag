061b2f6b7c5700efff5a0311ace835a46826c841
GandhiCloudLab <GandhiCloudLab@gmail.com>
2020-05-08 12:24:20 +0530

    Script Updated

--- install/1-create-tgz.sh
+++ install/1-create-tgz.sh
@@ -12,7 +12,7 @@ cp -r ./terraform/ ./temp/
 cd temp
 
 rm -rf .git .gitignore
-COPYFILE_DISABLE=1 tar czfv AccesssGroup-0.0.1.tgz accesssgroup/
+COPYFILE_DISABLE=1 tar czfv AccesssGroup-0.0.1.tgz accessgroup/
 COPYFILE_DISABLE=1 tar czfv AccesssGroup-AccessPolicy-0.0.1.tgz accessgroup-accesspolicy/
 
 mv *.tgz ../tgz
